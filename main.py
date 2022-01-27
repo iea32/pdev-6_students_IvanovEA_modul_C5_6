9f45b640cbc5aa21034ca45e9fb0ab1d043ab897
iea32 <iea32@mail.ru>
2022-01-27 17:32:53 +0500

    add test handler

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import telebot
+
+TOKEN = "5254187244:AAHvAXakpIHi6yKaFvHC4QFlg-KsOmLWCQM"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler()
+def echo_test(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "hello")
+
+bot.polling()
