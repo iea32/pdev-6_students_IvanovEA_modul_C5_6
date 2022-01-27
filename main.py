550476fee5f13805ec3c96e40ff3e50cc463555d
iea32 <iea32@mail.ru>
2022-01-27 21:52:13 +0500

    add 2 handler

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ TOKEN = "5254187244:AAHvAXakpIHi6yKaFvHC4QFlg-KsOmLWCQM"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, "hello")
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USD"
+}
+
+
+@bot.message_handler(commands=["start", "help"])
+def help(message: telebot.types.Message):
+    text = "Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество валюты>\nУвидить список всех доступных валют: /values"
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=["values"])
+def values(messege: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = "\n".join((text, key, ))
+    bot.reply_to(messege, text)
+
 
 bot.polling()
