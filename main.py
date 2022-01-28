c894b308d33eafd007c3eada5c4349e2046de004
iea32 <iea32@mail.ru>
2022-01-28 14:30:53 +0500

    edit bot.message_handler

--- main.py
+++ main.py
@@ -24,16 +24,21 @@ def values(messege: telebot.types.Message):
 
 @bot.message_handler(content_types=["text", ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(" ")
+    try:
+        values = message.text.split(" ")
 
-    if len(values) != 3:
-        raise ConvertionException("Неправильное количество параметров.")
+        if len(values) != 3:
+            raise ConvertionException("Неправильное количество параметров.")
 
-    quote, base, amount = values
-    total_base = CryptoConverter.convert(quote, base, amount)
-    # quote_ticker, base_ticker = keys[quote], keys[base]
-    text = f"Цена {amount} {quote} в {base} - {total_base}"
-    bot.send_message(message.chat.id, text)
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f"Ошибка пользователя.\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду\n{e}")
+    else:
+        text = f"Цена {amount} {quote} в {base} - {total_base}"
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
