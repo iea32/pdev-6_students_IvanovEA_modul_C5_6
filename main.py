30ff2003c431518c2cf8ad61b42d7a077ea315ee
iea32 <iea32@mail.ru>
2022-01-28 12:41:24 +0500

    add Exception

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ keys = {
     "доллар": "USD"
 }
 
+class ConvertionException(Exception)
+    pass
+
 
 @bot.message_handler(commands=["start", "help"])
 def help(message: telebot.types.Message):
@@ -31,8 +34,35 @@ def values(messege: telebot.types.Message):
 
 @bot.message_handler(content_types=["text", ])
 def convert(message: telebot.types.Message):
-    quote, base, amount = message.text.split(" ")
-    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}")
+    values = message.text.split(" ")
+
+    if len(values) > 3:
+        raise ConvertionException("Слишком много параметров.")
+
+    quote, base, amount = values
+
+    if quote == base
+        raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}.")
+
+    try:
+        quote_ticker = keys[quote]
+    except KeyError:
+        raise ConvertionException(f"Не удалось обработать валюту {quote}")
+
+    try:
+        base_ticker = keys[base]
+    except KeyError:
+        raise ConvertionException(f"Не удалось обработать валюту {quote}")
+
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise ConvertionException(f"Не удалось обработать количество {amount}")
+
+
+
+    quote_ticker, base_ticker = keys[quote], keys[base]
+    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
     total_base = json.loads(r.content)[keys[base]]
     text = f"Цена {amount} {quote} в {base} - {total_base}"
     bot.send_message(message.chat.id, text)
