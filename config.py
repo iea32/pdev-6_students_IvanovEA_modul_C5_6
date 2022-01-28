65c520b9b2fdca795c77b20ec3ce1533c05f5d8c
iea32 <iea32@mail.ru>
2022-01-28 13:36:20 +0500

    Refactoring

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN = "5254187244:AAHvAXakpIHi6yKaFvHC4QFlg-KsOmLWCQM"
+
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USD"
+}
--- main.py
+++ main.py
@@ -1,47 +1,9 @@
-import json
-import requests
 import telebot
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
 
-TOKEN = "5254187244:AAHvAXakpIHi6yKaFvHC4QFlg-KsOmLWCQM"
 
 bot = telebot.TeleBot(TOKEN)
-
-keys = {
-    "биткоин": "BTC",
-    "эфириум": "ETH",
-    "доллар": "USD"
-}
-
-class ConvertionException(Exception):
-    pass
-
-
-class CryptoConverter:
-    @staticmethod
-    def convert (quote: str, base: str, amount: str):
-
-        if quote == base:
-            raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}.")
-
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise ConvertionException(f"Не удалось обработать валюту {quote}")
-
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConvertionException(f"Не удалось обработать валюту {quote}")
-
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConvertionException(f"Не удалось обработать количество {amount}")
-
-        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
-        total_base = json.loads(r.content)[keys[base]]
-
-        return total_base
 
 
 @bot.message_handler(commands=["start", "help"])
